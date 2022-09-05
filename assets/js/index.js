dffdf7e0717bba5af490381e0b4641046df14359
Rodrigo-Covinha <Rodrigo-Covinha@users.noreply.github.com>
2022-09-04 23:29:51 -0300

    atualizações das paginas ATIV_MOD_3

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+ var button = document.getElementById("hamburguer");
+
+button.addEventListener("click", function(){
+    var listmenu = document.getElementById("listmenu");
+
+    listmenu.classList.toggle("hide"); 
+}) 
+
+
+//*var button = document.getElementById("hamburguer");
+//button.addEventListener("click", function(){
+//    var listmenu = document.getElementById("listmenu");
+
+//    if(listmenu.style.display === "none") {
+//        listmenu.style.display = "block";
+ //   }else {
+ //       listmenu.style.display = "none";
+ //   }
+
+//});   
